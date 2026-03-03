aa50917ef6897e3f79ef0b110072b9d148ad1d0a
jbl-paceline <john.lin@paceline.fit>
2026-03-03 11:05:33 -0800

    Update login.js

--- api/auth/google/login.js
+++ api/auth/google/login.js
@@ -1,14 +1,18 @@
 // Pepper - Google OAuth Login
-// Visit /api/auth/google/login in browser to start Google auth
+// Requests Calendar + Gmail read access
 
 export default function handler(req, res) {
   const params = new URLSearchParams({
     client_id: process.env.GOOGLE_CLIENT_ID,
     redirect_uri: process.env.GOOGLE_REDIRECT_URI,
     response_type: 'code',
-    scope: 'https://www.googleapis.com/auth/calendar.readonly',
+    scope: [
+      'https://www.googleapis.com/auth/calendar.readonly',
+      'https://www.googleapis.com/auth/gmail.readonly',
+    ].join(' '),
     access_type: 'offline',
     prompt: 'consent',
   });
+
   return res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`);
 }
